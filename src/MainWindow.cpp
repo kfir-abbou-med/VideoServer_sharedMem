1a98d76ad9c6653c09fae4cec0e27e6d7ae44097
kfir-abbou-med <kfir.abbou@medtronic.com>
2024-12-09 22:49:32 +0200

    camera windows removed from flow

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -28,6 +28,7 @@ void MainWindow::detectCameras()
     auto settingsManager = new VideoSettingsManager(*commService);
     int availableCameras[4] = {0, 4};
     int numCameras = 2;
+
     for (int i = 0; i < numCameras; ++i)
     {
         cv::VideoCapture testCapture;
@@ -38,35 +39,49 @@ void MainWindow::detectCameras()
         testCapture.set(cv::CAP_PROP_FPS, 60);
         testCapture.set(cv::CAP_PROP_FRAME_WIDTH, 640);
         testCapture.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
-        testCapture.open(camIndex, cv::CAP_V4L2);
+        // testCapture.open(camIndex, cv::CAP_V4L2);
 
-        if (!testCapture.isOpened())
-        {
-            cout << "Failed to open camera at index " << camIndex << endl;
-            continue;
-        }
+        // if (!testCapture.isOpened())
+        // {
+        //     cout << "Failed to open camera at index " << camIndex << endl;
+        //     continue;
+        // }
 
-        cv::Mat frame;
-        if (!testCapture.read(frame))
-        {
-            cout << "Cannot read from camera at index " << camIndex << endl;
-            testCapture.release();
-            continue;
-        }
+        // cv::Mat frame;
+        // if (!testCapture.read(frame))
+        // {
+        //     cout << "Cannot read from camera at index " << camIndex << endl;
+        //     testCapture.release();
+        //     continue;
+        // }
 
-        if (frame.empty())
-        {
-            cout << "Captured frame is empty at index " << camIndex << endl;
-            testCapture.release();
-            continue;
-        }
+        // if (frame.empty())
+        // {
+        //     cout << "Captured frame is empty at index " << camIndex << endl;
+        //     testCapture.release();
+        //     continue;
+        // }
 
-        cout << "Successfully opened camera at index: " << camIndex << endl;
-        cout << "Frame size: " << frame.cols << "x" << frame.rows << endl;
+        // cout << "Successfully opened camera at index: " << camIndex << endl;
+        // cout << "Frame size: " << frame.cols << "x" << frame.rows << endl;
 
-        CameraWindow *cameraWindow = new CameraWindow(camIndex, *settingsManager);
-        cameraWindow->show();
-        testCapture.release();
+        // CameraWindow *cameraWindow = new CameraWindow(camIndex, *settingsManager);
+        // cameraWindow->show();
+        // testCapture.release();
+
+        auto cameraWorker = new CameraWorker(camIndex, *settingsManager);
+        auto workerThread = new QThread;
+
+        cameraWorker->moveToThread(workerThread);
+        // Start the worker when the thread starts
+        connect(workerThread, &QThread::started, cameraWorker, &CameraWorker::start);
+
+        // Cleanup the worker and thread when the thread finishes
+        connect(workerThread, &QThread::finished, cameraWorker, &CameraWorker::deleteLater);
+        connect(workerThread, &QThread::finished, workerThread, &QThread::deleteLater);
+
+        // Start the thread
+        workerThread->start();
     }
 }
 
